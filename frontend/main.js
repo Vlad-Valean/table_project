637096c648ad8d2793f9e359dc83fc0527d6db38
Vlad Valean <vlad.valean19@gmail.com>
2022-01-24 00:03:31 +0200

    small changes
    menu-btn is now a checkbox
    better code in js file

--- frontend/main.js
+++ frontend/main.js
@@ -2,24 +2,15 @@ let act = document.getElementById("menu");
 let users = document.getElementById("users-span");
 let buy = document.getElementById("buy-span");
 let menu = document.querySelector(".menu");
-let container = document.querySelector(".content");
+let container = document.querySelector(".content")
 
-let val = 0;
-act.style.transition = 'transform 0.5s ease-in-out';
-menu.style.transition = 'flex 0.5s ease-in-out';
-users.style.transition = 'all 1s ease-in-out';
-buy.style.transition = 'all 1s ease-in-out';
 async function action() {
-    if(val == 0)
+    if(act.checked)
     {
-        val++;
-        act.style.transform = 'rotate(90deg)';
         menu.style.flex = '0';
         users.style.display = 'none';
         buy.style.display = 'none';
     } else {
-        val--;
-        act.style.transform = 'initial';
         if(container.style.flex != 'column') {
             menu.style.flex = '10%';
         }
